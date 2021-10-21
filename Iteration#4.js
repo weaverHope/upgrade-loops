d022dae0fe0013bfa223ae6530c88f7c125f73c8
weaverHope <92676579+weaverHope@users.noreply.github.com>
2021-10-21 18:46:18 +0200

    Create Iteration#4.js

---
+++ Iteration#4.js
@@ -0,0 +1,11 @@
+// Usa un for...in para imprimir por consola los datos del alienígena..Puedes usar este objeto:
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+
+for (let property in alien) {
+    console.log(property + ": " + alien[property]);
+}
