7d2b899e058f9bf7b00d1dcbc99b9bab1f2a388c
weaverHope <92676579+weaverHope@users.noreply.github.com>
2021-10-21 18:42:27 +0200

    Create Iteration#3.js

---
+++ Iteration#3.js
@@ -0,0 +1,8 @@
+// Usa un bucle forof para recorrer todos los destinos del array.Imprime en un console.log sus valores.
+
+// Puedes usar este array:
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+for (let i of placesToTravel) {
+    console.log(i);
+}
