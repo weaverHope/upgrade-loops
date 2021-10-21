7a14b3fc8d6a3b871fd81995ac20494f5f43fccb
weaverHope <92676579+weaverHope@users.noreply.github.com>
2021-10-21 12:40:27 +0200

    Create Iteration#1.js

---
+++ Iteration#1.js
@@ -0,0 +1,11 @@
+// Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta".
+// Usa la función.includes de javascript.
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
+
+for (let index = 0; index < products.length; index++) {
+    let aux;
+    aux = products[index].includes('Camiseta');
+    if (aux === true) {
+        console.log(products[index]);
+    }
+}
